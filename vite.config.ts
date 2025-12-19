6793e093886ba302e5d7deed8c2bf686ad7440a8
Bruno2202 <brunoterribile2005@gmail.com>
2025-12-19 09:15:00 -0300

    :wrench: Configure Vite host for network access

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert';
 
-// https://vite.dev/config/
 export default defineConfig({
+	server: {
+		https: false,
+	},
 	plugins: [
-		react(), 
-		tailwindcss()
+		react(),
+		tailwindcss(),
+		mkcert()
 	],
 })
