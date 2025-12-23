3925209388bd25e22cf0c2981a76e832046373cd
Bruno2202 <brunoterribile2005@gmail.com>
2025-12-23 10:00:33 -0300

    :wrench: Restrict Vite host to local environment
    • Updated vite.config.ts to run the project only locally
    • Removed network host access configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	server: {
-		https: false,
-	},
-	plugins: [
-		react(),
-		tailwindcss(),
-		mkcert()
-	],
+    server: {
+        port: 5173,
+    },
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
 })
