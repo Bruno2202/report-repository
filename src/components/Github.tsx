ff697ec368afc0f0997382dc9da135d007cdc4bc
Bruno2202 <brunoterribile2005@gmail.com>
2025-12-30 08:48:54 -0300

    :sparkles: Add GitHub access component

---
+++ src/components/Github.tsx
@@ -0,0 +1,17 @@
+import { GrGithub } from "react-icons/gr";
+
+const Github: React.FC = () => {
+    return (
+        <div className="flex flex-col gap-2 items-center justify-center text-dark-gray text-xs cursor-pointer">
+            <div
+                className="flex flex-row justify-center items-center gap-1 font-semibold hover:text-gray transition-all"
+                onClick={() => window.open('https://github.com/Bruno2202/report-repository')}
+            >
+                <GrGithub size={24} />
+                Github
+            </div>
+        </div>
+    );
+}
+
+export default Github;
